a5e67cc0055219cf376e48bf039eec6b85011173
Pubudu Gunatilaka <pubudu538@gmail.com>
2012-01-17 20:24:02 +0530

    Github  Assignment

--- Project/src/Pet.java
+++ Project/src/Pet.java
@@ -14,22 +14,3 @@ public class Pet {
 	
 	
 }
-
-public class Dog implements PetDog {
-
-	public void eat() {
-		System.out.println("I'm eating");
-	}
-
-	@Override
-	public void walk() {
-		System.out.println("I'm walking");
-	}
-
-	public int countLegs() {
-		// TODO Auto-generated method stub
-		return 4;
-	}
-
-	
-}
